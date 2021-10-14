7fe1e656e5bdbc7c9702864171ac32dafb120c2e
Robert Wolff <ro.wolff@gmx.net>
2021-10-14 15:08:00 +0200

    Add klarna sdk

--- src/klarna/Invoice.tsx
+++ src/klarna/Invoice.tsx
@@ -1,3 +1,16 @@
+import { useKlarna } from './useKlarna';
+
+const KLARNA_PAYMENTS_CONTAINER = 'klarna-payments-container';
+
 export const Invoice: React.FC = () => {
-  return <section>Buy Naow!</section>;
+  const { klarnaContainerRef, ready } = useKlarna(KLARNA_PAYMENTS_CONTAINER);
+
+  console.log(ready);
+
+  return (
+    <section>
+      <button>Buy Naow!</button>
+      <div id={KLARNA_PAYMENTS_CONTAINER} ref={klarnaContainerRef} />
+    </section>
+  );
 };
