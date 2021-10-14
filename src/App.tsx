885d22abca624bf85bd765d4f16d8b40b7f72e47
Robert Wolff <ro.wolff@gmx.net>
2021-10-14 14:24:24 +0200

    initial ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import logo from './logo.svg';
 
 import './App.css';
@@ -8,16 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>Click the link below to pay with invoice.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Buy naow!
         </a>
       </header>
     </div>
